3913373430ef524bd1832882b5c50ace8e6326a4
Mahisami <88837372+Mahisami@users.noreply.github.com>
2022-12-22 00:00:14 +0300

    Create kthSmallestElement.js

---
+++ kthSmallestElement.js
@@ -0,0 +1,17 @@
+
+const kthSmallest = (matrix, k) =>  {
+    
+  const calculateArray = (arr) => {
+    return arr.reduce(function (acc, currentVal) {
+      return acc.concat(
+        Array.isArray(currentVal) ? calculateArray(currentVal) : currentVal
+      );
+    }, []);
+  };
+
+  const result = calculateArray(matrix)
+    .sort((a, b) => a - b)
+    .filter((item, i) => i === k - 1);
+
+  return result;
+};
