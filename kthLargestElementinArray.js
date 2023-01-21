5eb0102162ea7c20843939984a09b76bb0345903
Mahisami <88837372+Mahisami@users.noreply.github.com>
2023-01-21 23:48:18 +0300

    Update kthLargestElementinArray.js

--- kthLargestElementinArray.js
+++ kthLargestElementinArray.js
@@ -1,11 +1,21 @@
-/**
- * @param {string[]} nums
- * @param {number} k
- * @return {string}
- */
-var kthLargestNumber = function (nums, k) {
-  let a = nums.map((x) => BigInt(x));
-  a.sort((a, b) => (a <= b ? 1 : -1));
+var KthLargest = function(k, nums) {
+    arr=nums.sort((a,b)=>b-a)
+    n=k
+};
 
-  return `${a[k - 1]}`;
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    let done = false
+    for(let q=arr.length-1;q>=0;q--){
+        if(val<arr[q]){
+            arr.splice(q+1,0,val)
+            done = true
+            break
+        }
+    }
+    if (!done)arr.splice(0,0,val)
+    return arr[n-1]
 };
